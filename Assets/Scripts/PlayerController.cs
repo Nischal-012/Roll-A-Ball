3f283a34faf4b9178f0eb76ffdbc1f09be1f943c
Nischal Joshi <076bct051@ioepc.edu.np>
2022-05-09 20:20:29 +0545

    Score/Count added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed;
+    public TextMeshProUGUI countText;
     private Rigidbody rb;
+    private int count;
     private float MovementX;
     private float MovementY;
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
        rb=GetComponent<Rigidbody>();
+        SetCountText();
     }
     void OnMove(InputValue MovementValue)
     {
@@ -22,6 +27,10 @@ public class PlayerController : MonoBehaviour
         MovementY = MovementVector.y;
 
     }
+    void SetCountText()
+	{
+        countText.text = "Count : " + count.ToString();    
+	}
 	private void FixedUpdate()
 	{
 		Vector3 Movement = new Vector3 (MovementX,0.0f,MovementY);
@@ -33,6 +42,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pickups"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
     }
 }
